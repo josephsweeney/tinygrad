635262ce5e9f59becbe999d199dfda04f80233e5
Joseph Sweeney <joe.sweeney@trailofbits.com>
2023-05-15 20:00:08 -0400

    Getting closer, though still broken winograd

--- extra/metal_winograd_conv.py
+++ extra/metal_winograd_conv.py
@@ -5,39 +5,29 @@ import numpy as np
 from tinygrad.helpers import dtypes, getenv
 from tinygrad.runtime.ops_metal import RawMetalBuffer, MetalProgram
 
-"""
-The idea is to have the each kernel call be for a single tile.
+BS = 64
+CIN = 1
+COUT = 1
+HW = 32
+K = 3
+assert(CIN==COUT) # For simplicity for now, can add groups later.
+FLOPS = BS*K*K*CIN*HW*HW*COUT*2
 
-D_t = (BD)B^T <- This will be done for each kernel call.
+output = RawMetalBuffer(BS*COUT*HW*HW, dtypes.float32)
 
-F_t = (GF)G^T <- This filter can be computed once and used everywhere.
-
-O_t = F_t (.) D_t <- Elementwise multiply, just a for loop.
-
-O = (AO_t)A^T
-
-"""
-
-XSIZE = 2048
-YSIZE = 1024
-
-# TODO: Get data from image
-output = RawMetalBuffer((XSIZE - 2)*(YSIZE - 2), dtypes.float32)
-debug = RawMetalBuffer(16, dtypes.float32)
-
-n_data = np.random.default_rng().standard_normal(size=(YSIZE, XSIZE), dtype=np.float32) #.astype(np.int32).astype(np.float32)
+n_data = np.random.default_rng().standard_normal(size=(BS,CIN,HW,HW), dtype=np.float32)
 
 # Box Blur
-n_conv = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.float32) * (1/9)
+# n_conv = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], dtype=np.float32) * (1/9)
+n_conv = np.random.default_rng().standard_normal(size=(COUT,CIN,K,K), dtype=np.float32)
 
 A = np.array([[1, 1, 1, 0], [0, 1, -1, -1]], dtype=np.float32)
 B = np.array([[1, 0, -1, 0], [0, 1, 1, 0], [0, -1, 1, 0], [0, 1, 0, -1]], dtype=np.float32)
 G = np.array([[1, 0, 0], [1/2, 1/2, 1/2], [1/2, -1/2, 1/2], [0, 0, 1]], dtype=np.float32)
-n_filter = np.matmul(np.matmul(G, n_conv), G.T)
+n_filter = np.array([[np.matmul(np.matmul(G, conv), G.T) for conv in c_conv] for c_conv in n_conv], dtype=np.float32)
+#TODO: Add padding before making metal buffer
 data = RawMetalBuffer.fromCPU(n_data)
-
-# FLOPS = N*N*N*2
-# BW = N*N*3*4
+filter_buf = RawMetalBuffer.fromCPU(n_filter)
 
 def shader_str(arr):
     return f"float{arr.shape[1]}x{arr.shape[0]}(" + ','.join(map(str, arr.T.flatten())) + ")" # .T is needed because shader is column major
@@ -51,43 +41,54 @@ constant float4x2 A =  {shader_str(A)};
 constant float2x4 AT = {shader_str(A.T)};
 constant float4x4 B =  {shader_str(B)};
 constant float4x4 BT = {shader_str(B.T)};
-constant float4x4 F_t = {shader_str(n_filter)};
 
-kernel void conv(device float *out, device const float *data, uint3 gid [[thread_position_in_grid]]) {{
-  data += (gid.x * 2) + ({XSIZE} * gid.y * 2);
+kernel void conv(device float *out, device const float *data, device const float *filter, uint3 gid [[thread_position_in_grid]]) {{
+  uint batch = gid.x;
+  uint cout = gid.y/{CIN};
+  uint cin = gid.y%{COUT};
+  filter += (cout * {CIN} * {K*K}) + (cin * {K*K});
+  float4x4 F_t = float4x4(filter[0],  filter[1],  filter[2],  filter[3],
+                          filter[4],  filter[5],  filter[6],  filter[7], 
+                          filter[8],  filter[9],  filter[10], filter[11], 
+                          filter[12], filter[13], filter[14], filter[15]);
+  uint x = (gid.z%{HW})*2;
+  uint y = (gid.z/{HW})*2;
+  data += batch * ({CIN*HW*HW}); // Get to the right batch
+  data += cin * ({HW*HW}); // Assume parallel channels rather than interspersed for simplicity
+  data += x + (y * {HW});
   float4x4 D;
   for (int i = 0; i < 4; ++i) {{
-    D[i] = float4(data[0], data[1], data[2], data[3]); 
-    data += {XSIZE};
+      D[i] = float4(data[0], data[1], data[2], data[3]); 
+      data += {HW};
   }}
   float4x4 D_t = ((B * D) * BT);
   float4x4 O_t = float4x4((F_t[0]*D_t[0]), (F_t[1]*D_t[1]), (F_t[2]*D_t[2]), (F_t[3]*D_t[3]));
   float2x2 O = (A * O_t) * AT;
-  uint index = (gid.x * 2) + ({XSIZE - 2} * gid.y * 2);
-  out[index]               = O[0][0];
-  out[index+1]             = O[0][1];
-  out[index+{XSIZE - 2}]   = O[1][0];
-  out[index+{XSIZE - 2}+1] = O[1][1];
+  out += (batch * {COUT*HW*HW}) + (cout * {HW*HW});
+  uint index = x + (y * {HW});
+  out[index]        = O[0][0];
+  out[index+1]      = O[0][1];
+  out[index+{HW}]   = O[1][0];
+  out[index+{HW}+1] = O[1][1];
 }}""")
 def timeit(fxn):
   st = time.perf_counter()
   et = fxn()
   # NOTE: et doesn't contain the launch overhead
   return time.perf_counter() - st
-tm = min([timeit(lambda: prog([((XSIZE-2)//2), ((YSIZE-2)//2), 1], [1, 1, 1], output, data, wait=True)) for _ in range(20)])
-local_output = output.toCPU().reshape(YSIZE-2,XSIZE-2)
-print(f"time: {tm}")
+tm = min([timeit(lambda: prog([BS, COUT*CIN, HW*HW], [1,1,1], output, data, filter_buf, wait=True)) for _ in range(20)])
+local_output = output.toCPU().reshape(BS,COUT,HW,HW)
+print(f"{tm*1e6:9.2f} us, would be {FLOPS*1e-9/tm:9.2f} GFLOPS conv in tinygrad")
 
-def winograd_np(data):
-    output = np.zeros(((YSIZE - 2), (XSIZE - 2)), dtype=np.float32)
-    for y in range(0, YSIZE-3, 2):
-        for x in range(0, XSIZE-3, 2):
-            O_t = np.multiply(np.matmul(np.matmul(B, data[y:y+4, x:x+4]), B.T), n_filter)
-            O = np.matmul(np.matmul(A, O_t), A.T)
-            output[y:y+2, x:x+2] = O
-    return output
+import time, torch, torch.mps
+b = torch.from_numpy(n_data).to('mps')
+c = torch.from_numpy(n_conv).to('mps')
 
-cpu_out = winograd_np(n_data)
-np.testing.assert_allclose(local_output, cpu_out, atol=1e-3)
-# TODO: Compute FLOPs, and estimate optimal FLOPS.
-# TODO: Imitate tinygrad and pytorch API for shader
+def torch_prog(b, c):
+  st = time.perf_counter()
+  a = torch.nn.functional.conv2d(b, c, padding=1)
+  torch.mps.synchronize()
+  np.testing.assert_allclose(local_output, a.cpu(), atol=1e-3)
+  return time.perf_counter() - st
+tm = min([torch_prog(b, c) for _ in range(20)])
+print(f"{tm*1e6:9.2f} us, would be {FLOPS*1e-9/tm:9.2f} GFLOPS conv in torch")
